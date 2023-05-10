73b6e5849dfe384a6586b2a25559ff61db888172
Yoac-eng <cayocutrim1991@gmail.com>
2023-05-10 16:34:53 -0300

    First commit

---
+++ src/test/java/com/yoacspring/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoacspring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
